0ae9a9cf8e4ced54bf56a533da7f4cc9100af928
chiheb <chiheb.elouekdi@gmail.com>
2018-11-14 15:49:51 -0500

    ok

--- P2-DetectLangue.py
+++ P2-DetectLangue.py
@@ -37,7 +37,8 @@ def Attributs_phrases(phrase,freq_dist,nbgrm):
 def Preparer_Attribut_List(chemin_fichier, nb_ligne, langue, ngramme):
 
     file = open(chemin_fichier, 'rb')
-    txt_lect = [ligne for ligne in file]
+    txt_lct = [ligne for ligne in file]
+    txt_lect = [x.lower() for x in txt_lct]
     txt_ngram_train = list(nltk.ngrams(txt_lect,ngramme))
     txt_freq_dist_ngram = FreqDist(txt_ngram_train)
     file.close()
@@ -61,7 +62,8 @@ def Reconnaitre_langue(fichier, classificateur,nb_ligne, ngramme):
     f = open(fichier, 'rb')
 
     testset=[]
-    txt_lect = [ligne for ligne in f]
+    txt_lct = [ligne for ligne in f]
+    txt_lect = [x.lower() for x in txt_lct]
     txt_ngram_train = list(nltk.ngrams(txt_lect, ngramme))
     txt_freq_dist_ngram = FreqDist(txt_ngram_train)
     f.close()
@@ -82,48 +84,89 @@ def Reconnaitre_langue(fichier, classificateur,nb_ligne, ngramme):
         if count_langue[x] > nb_phrase_langue:
             langue_texte = x
             nb_phrase_langue = count_langue[x]
+  #  print(count_langue)
     return langue_texte
 
 
-NB_LIGNES=5
-N_GRAMMES =4
+
+
 Chemin_fich_en = 'identification_langue/corpus_entrainement/english-training.txt'
 Chemin_fich_es = 'identification_langue/corpus_entrainement/espanol-training.txt'
 Chemin_fich_fr = 'identification_langue/corpus_entrainement/french-training.txt'
 Chemin_fich_pt = 'identification_langue/corpus_entrainement/portuguese-training.txt'
 
+f = open('output.txt','w')
+stdout_old = sys.stdout
+sys.stdout = f
+#sys.stdout = stdout_old
 
-trainset = Preparer_Attribut_List(Chemin_fich_en,NB_LIGNES,"en",N_GRAMMES) \
-           +  Preparer_Attribut_List(Chemin_fich_es,NB_LIGNES,"es",N_GRAMMES)\
-           +  Preparer_Attribut_List(Chemin_fich_fr,NB_LIGNES,"fr",N_GRAMMES)\
-           + Preparer_Attribut_List(Chemin_fich_pt,NB_LIGNES,"pt",N_GRAMMES)
-
-
-classifier = NaiveBayesClassifier.train(trainset)
-
-Chemin_fich_test = "identification_langue/corpus_test1/"
-
-
-p1 = re.compile(r'.*(fr|en|es|pt)\.txt')
-
-testset=[]
-accuracy = 0
-for fabc in os.listdir(Chemin_fich_test):
-    m1 = p1.search(fabc)
-    if m1 is not None:
-
-        testset += Preparer_Attribut_List(Chemin_fich_test+fabc, NB_LIGNES, m1.group(1), N_GRAMMES)
-
-        #print(fabc + "::" + Reconnaitre_langue(Chemin_fich_test + fabc, classifier,NB_LIGNES,N_GRAMMES))
-        if Reconnaitre_langue(Chemin_fich_test + fabc, classifier,NB_LIGNES,N_GRAMMES) == m1.group(1):
-
-            accuracy +=1
-
-print (nltk.classify.util.accuracy(classifier, testset))
-
-print (float(accuracy))
-
-classifier.show_most_informative_features()
+for n in range(1,4):
+    N_GRAMMES = n
+    for m in range (1,11):
+        NB_LIGNES = m
+        print("-----------------------------"+ n.__str__() + " :: "+m.__str__()+"-----------------------------------------")
+        trainset = Preparer_Attribut_List(Chemin_fich_en,NB_LIGNES,"en",N_GRAMMES) \
+                   +  Preparer_Attribut_List(Chemin_fich_es,NB_LIGNES,"es",N_GRAMMES)\
+                   +  Preparer_Attribut_List(Chemin_fich_fr,NB_LIGNES,"fr",N_GRAMMES)\
+                   + Preparer_Attribut_List(Chemin_fich_pt,NB_LIGNES,"pt",N_GRAMMES)
 
 
+        classifier = NaiveBayesClassifier.train(trainset)
+
+        lg_classifier = MaxentClassifier.train(trainset, max_iter=4)
+
+
+
+        Chemin_fich_test = "identification_langue/corpus_test1/"
+
+
+        p1 = re.compile(r'.*(fr|en|es|pt)\.txt')
+
+        print("------------------------------------NAIVE BAYES--------------------------------------")
+
+        testset=[]
+        accuracy = 0
+        for fabc in os.listdir(Chemin_fich_test):
+            m1 = p1.search(fabc)
+            if m1 is not None:
+
+                testset += Preparer_Attribut_List(Chemin_fich_test+fabc, NB_LIGNES, m1.group(1), N_GRAMMES)
+
+                #print(fabc + "::" + Reconnaitre_langue(Chemin_fich_test + fabc, classifier,NB_LIGNES,N_GRAMMES))
+                if Reconnaitre_langue(Chemin_fich_test + fabc, classifier,NB_LIGNES,N_GRAMMES) == m1.group(1):
+
+                    accuracy +=1
+
+        print (nltk.classify.util.accuracy(classifier, testset))
+
+        print (float(accuracy))
+
+        classifier.show_most_informative_features()
+        print ("------------------------------LOGISTIC REGRESSION-------------------------------------")
+
+        testset=[]
+        accuracy = 0
+        for fabc in os.listdir(Chemin_fich_test):
+            m1 = p1.search(fabc)
+            if m1 is not None:
+
+                testset += Preparer_Attribut_List(Chemin_fich_test+fabc, NB_LIGNES, m1.group(1), N_GRAMMES)
+
+                #print(fabc + "::" + Reconnaitre_langue(Chemin_fich_test + fabc, classifier,NB_LIGNES,N_GRAMMES))
+                if Reconnaitre_langue(Chemin_fich_test + fabc, lg_classifier,NB_LIGNES,N_GRAMMES) == m1.group(1):
+
+                    accuracy +=1
+
+        print (nltk.classify.util.accuracy(lg_classifier, testset))
+
+        print (float(accuracy))
+
+        classifier.show_most_informative_features()
+        print ("-------------------------------------------------------------------------------------")
+
+
+
+
+sys.stdout = stdout_old
+print ("OK")
 
